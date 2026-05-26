0e9a2d93283bfe2d1b6acb6cfd6202ad32eacf89
Gabriel Malo <malo.gabriel.2@gmail.com>
2026-05-26 18:40:31 -0400

    Initial commit

---
+++ DeepFryerCli/FileCompressor.cs
@@ -0,0 +1,14 @@
+﻿using FFMpegCore;
+
+namespace DeepFryerCli;
+
+public class FileCompressor(string inputFilePath, string outputFilePath)
+{
+    public async Task<bool> CompressAndOutputAsync() =>
+        await FFMpegArguments
+            .FromFileInput(inputFilePath)
+            .OutputToFile(outputFilePath, addArguments: options => options
+                .WithAudioBitrate(10)
+                .WithVideoBitrate(30)
+            ).ProcessAsynchronously();
+}
---
+++ DeepFryerCli/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+
+namespace DeepFryerCli;
+
+internal static class Program
+{
+    private static async Task<int> Main(string[] args)
+    {
+        Option<FileInfo> inputFileOption = new("--input", "-i")
+        {
+            Description = "File to deep fry",
+            Required = true
+        };
+
+        Option<string> outputPathOption = new("--output", "-o")
+        {
+            Description = "Output file",
+            Required = true
+        };
+
+        RootCommand rootCommand = new("Deep Fryer made for CLI")
+        {
+            inputFileOption,
+            outputPathOption
+        };
+
+        ParseResult parseResult = rootCommand.Parse(args);
+        if (parseResult.Errors.Count != 0)
+        {
+            foreach (ParseError error in parseResult.Errors) 
+                await Console.Error.WriteLineAsync(error.Message);
+
+            return 1;
+        }
+
+        if (parseResult.GetValue(inputFileOption) is not { } inputFile ||
+            parseResult.GetValue(outputPathOption) is not { } outputPath)
+        {
+            await Console.Error.WriteLineAsync("Please enter a valid file input.");
+            return 1;
+        }
+
+        FileCompressor compressor = new(inputFile.FullName, outputPath);
+        bool success = await compressor.CompressAndOutputAsync();
+
+        if (!success)
+        {
+            await Console.Error.WriteLineAsync("There was an error processing the file.");
+            return 1;
+        }
+            
+        Console.WriteLine("File has been deep fried!");
+        return 0;
+    }
+}
